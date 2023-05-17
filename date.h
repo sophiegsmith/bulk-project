429890cc88025dec789f9bde24472b8d9cccb41b
wwill24 <97563731+wwill24@users.noreply.github.com>
2023-05-16 19:13:10 -0700

    Create date.h

---
+++ date.h
@@ -0,0 +1,16 @@
+#ifndef DATE_H
+#define DATE_H
+#include <string>
+
+using namespace std;
+
+struct Date
+{
+    int month;
+    int date;
+    int year;
+};
+
+bool operator==(const Date& lhs, const Date& rhs);
+
+#endif
