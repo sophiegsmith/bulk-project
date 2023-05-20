95145748523753e82b9ab2aaa776a653ee0af859
wwill24 <97563731+wwill24@users.noreply.github.com>
2023-05-20 14:19:14 -0700

    adminmainpaige.cpp

--- adminmainpaige.cpp
+++ adminmainpaige.cpp
@@ -1,1 +1,29 @@
+#include "adminmainpage.h"
+#include "ui_adminmainpage.h"
+
+adminMainPage::adminMainPage(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::adminMainPage)
+{
+    ui->setupUi(this);
+}
+
+adminMainPage::~adminMainPage()
+{
+    delete ui;
+}
+
+void adminMainPage::on_pushButton_2_members_clicked()
+{
+    info = new memberinfo(this);
+    info -> show();
+}
+
+
+void adminMainPage::on_pushButton_sales_clicked()
+{
+    report = new sales(this);
+    report -> show();
+}
+
 
