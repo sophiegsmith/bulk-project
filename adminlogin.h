ddf3e54f1a9f9194f120ed9d75be2762ad063c2f
wwill24 <97563731+wwill24@users.noreply.github.com>
2023-05-20 14:18:10 -0700

    adminlogin.h

--- adminlogin.h
+++ adminlogin.h
@@ -1,1 +1,30 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include <QMainWindow>
+#include "adminmainpage.h"
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class admin; }
+QT_END_NAMESPACE
+
+class admin : public QMainWindow
+
+{
+    Q_OBJECT
+
+public:
+    admin(QWidget *parent = nullptr);
+    ~admin();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::admin *ui;
+    adminMainPage *adminMP;
+};
+
+#endif // ADMIN_H
 
